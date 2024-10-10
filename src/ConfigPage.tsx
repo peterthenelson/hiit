ea5a68469381e776ec13c89ad6600941a5737f37
peterthenelson <nelson.peter.a@gmail.com>
2024-10-09 22:36:05 -0400

    Replace text box with very basic drag-and-drop

--- src/ConfigPage.tsx
+++ src/ConfigPage.tsx
@@ -1,22 +1,6 @@
 import { SetStateAction, useState } from 'react';
 import { Page, TimerConfig } from './config';
-
-const defaultText = (
-  'Lat pulls\n' +
-  'Face pulls\n' +
-  'Rows\n' +
-  'Lateral Raises\n' +
-  'Jumping Jacks\n' +
-  'Shoulder Shrugs\n' +
-  'Overhead Press\n' +
-  'Bicep Curls\n' +
-  'Lunges\n' +
-  'Sumo Squats\n' +
-  'Dumbell Swings\n' +
-  'Half-kneeling Rows\n' +
-  'Chest Fly\n' +
-  'Clamshells\n' +
-  'Chest Press');
+import { DragDropContext, Droppable, Draggable, DropResult } from "@hello-pangea/dnd";
 
 export interface ConfigPageProps {
   config: TimerConfig,
@@ -24,20 +8,60 @@ export interface ConfigPageProps {
   setPage: React.Dispatch<SetStateAction<Page>>;
 }
 
-export function ConfigPage({ config, setConfig, setPage }: ConfigPageProps) {
-  const [exerciseText, setExerciseText] = useState(defaultText);
-  const handleText = (event: React.ChangeEvent<HTMLTextAreaElement>) => {
-    const et = event.target.value;
-    setExerciseText(et);
-    setConfig({ exercises: et.split('\n').filter(Boolean) });
+interface Item {
+  id: string,
+  content: string,
+}
+
+function exercisesToItems(exercises: string[]): Item[] {
+  const items: Item[] = [];
+  for (let i = 0; i < exercises.length; i++) {
+    items.push({ id: i.toString(), content: exercises[i] });
   }
+  return items;
+}
+
+function itemsToExercises(items: Item[]): string[] {
+  return items.map((i) => i.content);
+}
+
+export function ConfigPage({ config, setConfig, setPage }: ConfigPageProps) {
+  const [items, setItems] = useState(exercisesToItems(config.exercises));
+
+  function handleDragEnd(result: DropResult) {
+    if (!result.destination) return;
+    const tmp = Array.from(items);
+    const [reorderedItem] = tmp.splice(result.source.index, 1);
+    tmp.splice(result.destination.index, 0, reorderedItem);
+    setItems(tmp);
+    setConfig({ ...config, exercises: itemsToExercises(tmp) });
+  }
+
   return (
     <div>
       <div>
         <h1>HIIT Timer</h1>
-        <textarea
-         value={exerciseText}
-         onChange={handleText}/>
+        <DragDropContext onDragEnd={handleDragEnd} >
+          <Droppable droppableId='exercises-droppable'>
+            {(provided) => (
+              <div ref={provided.innerRef} {...provided.droppableProps}>
+                {items.map((item, index) => (
+                  <Draggable key={item.id} draggableId={item.id} index={index}>
+                    {(provided, snapshot) => (
+                      <div
+                       ref={provided.innerRef}
+                       style={provided.draggableProps.style}
+                       {...provided.draggableProps}
+                       {...provided.dragHandleProps}>
+                        {item.content}
+                      </div>
+                    )}
+                  </Draggable>
+                ))}
+              </div>
+            )}
+          </Droppable>
+        </DragDropContext>
         <p></p>
         <button onClick={() => setPage(Page.Timer)}>Start</button>
       </div>
